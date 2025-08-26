92f0d5e22d767ea4cc6eb63101d7a9177e8c7b07
LopesSoh <sophiac.lopes2008@gmail.com>
2025-08-26 08:19:29 -0300

    inicio do cadastro

--- funcao.py
+++ funcao.py
@@ -3,3 +3,11 @@ import os
 def ls():
     os.system('pause')
     os.system('cls')
+
+# função para cadastro de novos livros
+def cadastro(livros, nome,autor, ):
+    print('---VOCÊ ESCOLHEU CADASTRO---')
+    nome = input('Qual o título: \n')
+    autor = input('Qual o autor: \n')
+    genero_literario = input('Qual o gênero literário: \n')
+    editora = input('Qual a editora: \n')
