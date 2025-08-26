bda352b35a5d9e283b0329b19216fb8b32a4652e
LopesSoh <sophiac.lopes2008@gmail.com>
2025-08-26 09:00:32 -0300

    ls

--- funcao.py
+++ funcao.py
@@ -7,13 +7,14 @@ def ls():
 def menu():
     print("Olá, seja bem-vindo(a) a nossa biblioteca! =)")
     print("Digite:")
-    print("1-Cadastre um livro \n 2-Listar os livros pelo genero \n 3- Listar os livros pelo autor \n 4-Listar por livros empretados \n 5-Fazer a devolução de um livro\n 6-empretar livro\n 0-Sair")
+    print("\n1-Cadastre um livro \n 2-Listar os livros pelo genero \n 3- Listar os livros pelo autor \n 4-Listar por livros empretados \n 5-Fazer a devolução de um livro\n 6-Emprestar livro\n 0-Sair")
 
 # função para cadastro de novos livros
-def cadastro(livros, Livros):
+def cadastro(Livros, livros):
     print('---VOCÊ ESCOLHEU CADASTRO---')
     titulo = input('Qual o título: \n')
     autor = input('Qual o autor: \n')
     genero_literario = input('Qual o gênero literário: \n')
     editora = input('Qual a editora: \n')
     livros[len(livros) + 1] = Livros( Nome= titulo, Autor = autor, Genero = genero_literario, Editora = editora  )
+    ls()
