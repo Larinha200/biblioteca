1066b28c3ca16a8545063b6abd3f65682ec4b8ce
Larinha200 <laravitoriaqueiroz4@gmail.com>
2025-08-26 08:31:49 -0300

    class livros

---
+++ classes.py
@@ -0,0 +1,7 @@
+class Livros:
+     def __init__(self,Nome,Autor,Genero,Status):
+          self.__Nome__ = Nome
+          self.__Autor__ = Autor
+          self.__Genero__ = Genero
+          self.__Status__ = Status
+        
