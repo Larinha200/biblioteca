7f7e5f1134ee775143dec932b36c97e46b63d13b
LopesSoh <sophiac.lopes2008@gmail.com>
2025-08-26 10:36:31 -0300

    def identados

--- classes.py
+++ classes.py
@@ -1,5 +1,5 @@
 class Livros:
-     def __init__(self,Nome,Autor,Genero,Editora,Status):
+    def __init__(self,Nome,Autor,Genero,Editora,Status):
           self.__Nome__ = Nome
           self.__Autor__ = Autor
           self.__Genero__ = Genero
@@ -7,37 +7,37 @@ class Livros:
           self.__Status__ = Status
 
 # get e set
-def GetNome (self):
-    return self.__Nome__ 
+    def GetNome (self):
+        return self.__Nome__ 
     
-def GetAutor (self):
-    return  self.__Autor__
+    def GetAutor (self):
+        return  self.__Autor__
     
-def GetGenero (self):
-    return self.__Genero__
+    def GetGenero (self):
+        return self.__Genero__
      
-def GetEditora (self):
-    return self.__Editora__
+    def GetEditora (self):
+        return self.__Editora__
     
-def GetStatus (self):
-    return self.__Status__ 
+    def GetStatus (self):
+        return self.__Status__ 
     
-def SetNome (self, Nome):
-    self.__Nome__= Nome
-    return self.__Nome__ 
+    def SetNome (self, Nome):
+        self.__Nome__= Nome
+        return self.__Nome__ 
     
-def SetAutor (self,Autor):
-    self.__Autor__ = Autor
-    return  self.__Autor__
+    def SetAutor (self,Autor):
+        self.__Autor__ = Autor
+        return  self.__Autor__
     
-def SetGenero (self, Genero):
-    self.__Genero__ = Genero
-    return self.__Genero__
+    def SetGenero (self, Genero):
+        self.__Genero__ = Genero
+        return self.__Genero__
     
-def SetEditora (self, Editora):
-    self.__Editora__ = Editora
-    return self.__Editora__
+    def SetEditora (self, Editora):
+        self.__Editora__ = Editora
+        return self.__Editora__
     
-def SetStatus (self, Status):
-    self.__Status__ = Status
-    return self.__Status__   
+    def SetStatus (self, Status):
+        self.__Status__ = Status
+        return self.__Status__   
